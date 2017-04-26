0df846b6f40b03853e5923fcc913212f4486ce59
Antonio Calanducci <calanducci@etnatraining.it>
2017-04-26 11:59:17 +0200

    refactor Product with Functional component

--- src/OrderItem.js
+++ src/OrderItem.js
@@ -12,11 +12,36 @@ export default class OrderItem extends Component {
     // }
     state = { quantita: 0 }
 
+    // componentWillMount() {
+    //   console.log("sto per montare OrderItem");
+    // }
+    //
+    // componentDidMount() {
+    //   console.log("ho montato OrderItem");
+    // }
+    //
+    // shouldComponentUpdate(nextProps, nextState) {
+    //   console.log("vuoi aggiornare il componente? ");
+    //   //console.log(nextProps, nextState);
+    //   if (this.state.quantita > 0 ) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
+    //
+    // componentWillUpdate(nextProps, nextState) {
+    //   console.log("il componente si aggiornerà", nextProps, nextState);
+    // }
+    //
+    // componentDidUpdate(prevProps, prevState) {
+    //   console.log("il componente è stato aggiornernatp", prevProps, prevState);
+    // }
 
     render() {
         const subtotale = (this.props.price * this.state.quantita).toFixed(2);
 
-        console.log("sto elaborando il JSX e facendo il diff con il virtual DOM")
+        //console.log("sto elaborando il JSX e facendo il diff con il virtual DOM")
         return (
             <div style={{
                 //backgroundColor: "red",
@@ -30,27 +55,28 @@ export default class OrderItem extends Component {
                 <Thumbnail url={this.props.image}/>
                 <Product name={this.props.product} price={this.props.price}/>
                 <div style={{margin: 20}}>
-                    <div>Quantità: {this.state.quantita}</div>
-                    <div>
-                        <button onClick={() => {
-                            this.setState({quantita: this.state.quantita + 1})
-                            this.props.onQuantityChange(this.props.price)
-                          }
-                          }>
-                          Add
-                        </button>
-                        <button onClick={() => {
-                            if (this.state.quantita > 0) {
-                              this.setState({quantita: this.state.quantita - 1});
-                              this.props.onQuantityChange(-this.props.price);
-                            }
-
-                          }
-                          }>
-                          Remove
-                        </button>
-                        <h5>Subtotale: {subtotale}</h5>
-                    </div>
+                  <div>
+                    Quantità: {this.state.quantita}
+                  </div>
+                  <div>
+                    <button onClick={() => {
+                        this.setState({quantita: this.state.quantita + 1});
+                        this.props.onQuantityChange(this.props.price);
+                      }}>
+                      Add
+                    </button>
+                    <button onClick={() => {
+                        if (this.state.quantita > 0) {
+                          this.setState({quantita: this.state.quantita - 1});
+                          this.props.onQuantityChange(-this.props.price);
+                        }
+                      }}>
+                      Remove
+                    </button>
+                    <h5>
+                      Subtotale: {subtotale}
+                    </h5>
+                  </div>
                 </div>
             </div>
 
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-/*export default class Product extends Component {
-    render() {
-        return (
-            <div>
-                <h3>{this.props.name}</h3>
-                <h4>{this.props.price} Euro</h4>
-            </div>
-        )
-    }
-}*/
+// export default class Product extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>{this.props.name}</h3>
+//                 <h4>{this.props.price} Euro</h4>
+//             </div>
+//         )
+//     }
+// }
+
+// function Product(props) {
+//   return (
+//     <div>
+//         <h3>{this.props.name}</h3>
+//         <h4>{this.props.price} Euro</h4>
+//     </div>
+//   );
+// }
 
 const Product = (props) => (
-    <div>
-        <h3>{props.name}</h3>
-        <h4>{props.price} Euro</h4>
-    </div>
+  <div>
+    <h3>{props.name}</h3>
+    <h4>{props.price} Euro</h4>
+  </div>
 );
 
 Product.propTypes = {
